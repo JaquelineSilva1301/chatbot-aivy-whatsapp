b1030af80d55db0550d573bea8ec0b0047743350
JaquelineSilva1301 <jaquelines130614@gmail.com>
2026-02-24 10:38:38 -0300

    Criar flows/saude.js

---
+++ src/flows/saude.js
@@ -0,0 +1,7 @@
+function saude() {
+  console.log("ODS 3 - Saúde e Bem-Estar");
+  console.log("Objetivo: Garantir uma vida saudável e promover o bem-estar para todos.");
+  console.log("Importância: Acesso à saúde de qualidade reduz desigualdades e aumenta a qualidade de vida.");
+}
+
+module.exports = saude;
