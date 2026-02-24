f0e34d2ee91e9125b0d180ac3a01be3b0c5fc4f7
JaquelineSilva1301 <jaquelines130614@gmail.com>
2026-02-24 10:40:32 -0300

    Criar igualdadegenero.js

---
+++ src/flows/igualdadegenero.js
@@ -0,0 +1,16 @@
+function fluxoIgualdadeGenero(message) {
+    message.reply(
+`⚖ Igualdade de Gênero
+
+A igualdade de gênero é um dos Objetivos de Desenvolvimento Sustentável (ODS 5).
+
+✔ Direitos iguais
+✔ Oportunidades iguais
+✔ Combate à violência
+✔ Empoderamento feminino
+
+Digite "menu" para voltar.`
+    );
+}
+
+module.exports = fluxoIgualdadeGenero;
