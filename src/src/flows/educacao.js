88ccaec853e6699080be5d2f28f628504cf60b18
JaquelineSilva1301 <jaquelines130614@gmail.com>
2026-02-24 10:32:42 -0300

    Criar educacao.js

---
+++ src/src/flows/educacao.js
@@ -0,0 +1,16 @@
+function fluxoEducacao(message) {
+    message.reply(
+`📚 Educação
+
+Você pode acessar nossos quizzes educativos no projeto Praticaí!
+
+Temas disponíveis:
+✔ Português
+✔ Matemática
+✔ Inglês
+
+Digite "menu" para voltar.`
+    );
+}
+
+module.exports = fluxoEducacao;
